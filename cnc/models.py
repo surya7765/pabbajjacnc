9eae65e8662707fa787e856f07d33b0aebe8976f
Mehul Kumar <mehulkumar7858.983524@gmail.com>
2021-12-10 00:31:45 +0530

    commiting all the change of saturday and sunday

--- cnc/models.py
+++ cnc/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.utils.text import slugify
 
 # Create your models here.
 
@@ -10,3 +11,19 @@ class Review(models.Model):
     published_date = models.DateTimeField(default=timezone.now)
     image = models.ImageField(
         upload_to='upload/', height_field=None, width_field=None, max_length=None)
+
+
+class Product(models.Model):
+    title = models.CharField(("Give your title"), max_length=50)
+    manufacturing_date = models.DateField(
+        ("Add Manufacturing date"), auto_now=False, auto_now_add=False)
+    price = models.IntegerField(("Enter Price"))
+    availability = models.BooleanField(("Available or Not"))
+    item_number = models.CharField(max_length=50)
+    model_numbeer = models.CharField(max_length=50)
+
+
+class ProductImage(models.Model):
+    title = models.ForeignKey(Product, default=None,
+                              on_delete=models.CASCADE, related_name='images')
+    image = models.ImageField(upload_to='upload/')
